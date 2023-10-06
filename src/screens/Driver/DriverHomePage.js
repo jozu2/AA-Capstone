844d5563cfe882bf8d29e6c2b1e3758e23acf17d
jozu2 <webdevjosu@gmail.com>
2023-10-06 16:55:43 +0800

    RE

--- src/screens/Driver/DriverHomePage.js
+++ src/screens/Driver/DriverHomePage.js
@@ -85,7 +85,7 @@ const DriverHomePage = () => {
       ]
     );
   };
-  handleCreateButton = () => {
+  const handleCreateButton = () => {
     navigation.navigate("DriverRideRegistration");
   };
   return (
